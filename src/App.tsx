dafdbeca403a23404b9264aab0f9f8a0ced81081
JJ_EMPTY_STRING <JJ_EMPTY_STRING>
2026-03-11 00:13:33 +0900

    dockui ok but render problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import * as FlexLayout from "flexlayout-react"
 import "flexlayout-react/style/dark.css"
@@ -11,59 +11,73 @@ import SpectrumPlot from "./plots/components/SpectrumPlot"
 import { DetectionTable } from "./tables/components/DetectionTable"
 
 const layoutJson = {
-  global: {
-      tabSetEnableDrop: false,
-      tabEnableClose: false,
-      tabSetEnableDrag: false,
-      tabEnableDrag: false,
-      tabEnableRename: false
+  "global": {
+    "tabEnableClose": false,
+    "tabEnableRename": false,
+    "tabSetEnableDrop": false
   },
-  borders: [],
-  layout: {
-    type: "row",
-    weight: 100,
-    children: [
+  "borders": [],
+  "layout": {
+    "type": "row",
+    "id": "#126f4dd8-6f4f-4e6c-a7fc-64da0db52861",
+    "children": [
       {
-        type: "tabset",
-        weight: 20,
-        children: [
+        "type": "row",
+        "id": "#6ea25519-1fc1-41b2-8b1a-3bf75f01b081",
+        "weight": 50,
+        "children": [
           {
-            type: "tab",
-            name: "Control",
-            component: "control"
-          }
-        ]
-      },
-      {
-        type: "column",
-        weight: 80,
-        children: [
-          {
-            type: "tabset",
-            weight: 50,
-            children: [
+            "type": "tabset",
+            "id": "#8183ef08-d18b-4bc4-92b4-f40d6618b717",
+            "weight": 8.091908091908092,
+            "children": [
               {
-                type: "tab",
-                name: "Spectrum",
-                component: "spectrum"
+                "type": "tab",
+                "id": "#5f4c92ad-d08c-49c7-86e1-69adb71e241f",
+                "name": "Control",
+                "component": "control"
               }
             ]
           },
           {
-            type: "tabset",
-            weight: 50,
-            children: [
+            "type": "row",
+            "id": "#7a7472e5-b620-41bb-92bc-5cf91bd83744",
+            "weight": 91.9080919080919,
+            "children": [
               {
-                type: "tab",
-                name: "Detections",
-                component: "detection"
+                "type": "tabset",
+                "id": "#35529e78-0796-45aa-8725-543986c0b24d",
+                "weight": 50,
+                "children": [
+                  {
+                    "type": "tab",
+                    "id": "#25c4fa55-4e38-4d94-b890-8992c710dee5",
+                    "name": "Spectrum",
+                    "component": "spectrum"
+                  }
+                ]
+              },
+              {
+                "type": "tabset",
+                "id": "#1a126141-cce6-4e96-8fd5-c477690c54df",
+                "weight": 50,
+                "children": [
+                  {
+                    "type": "tab",
+                    "id": "#5dd970d8-2747-46b6-8344-2de5f704ab37",
+                    "name": "Detections",
+                    "component": "detection"
+                  }
+                ],
+                "active": true
               }
             ]
           }
         ]
       }
     ]
-  }
+  },
+  "popouts": {}
 }
 
 export default function App() {
@@ -74,7 +88,6 @@ export default function App() {
 
   const factory = (node: FlexLayout.TabNode) => {
     const component = node.getComponent()
-
     // return <div className="placeholder">{component}</div>
     console.log("factory", component) 
     if (component === "control") return (
@@ -95,16 +108,33 @@ export default function App() {
       </div>
     )
 
+
     return <div className="placeholder">{component}</div>
   }
 
   return (
     <>
       <div style={{ height: "100vh" }}>
-        <FlexLayout.Layout model={model.current} factory={factory} />
+        <FlexLayout.Layout model={model.current} factory={factory}
+        // onModelChange={(m, a) => saveLayout(m)} 
+        />
       </div>
 
       <WaveformDialog />
     </>
   )
 }
+
+// function saveLayout(model: FlexLayout.Model) {
+//   const json = JSON.stringify(model.toJson(), null, 2)
+
+//   const blob = new Blob([json], { type: "application/json" })
+//   const url = URL.createObjectURL(blob)
+
+//   const a = document.createElement("a")
+//   a.href = url
+//   a.download = "layout.json"
+//   a.click()
+
+//   URL.revokeObjectURL(url)
+// }
